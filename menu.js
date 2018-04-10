3d92c9d5b4262543c9ade3e42f42a07723643472
Your Name <you@example.com>
2018-04-10 21:12:23 +0300

    menu

--- menu.js
+++ menu.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-   alert ("Ok")
+document.addEventListener('DOMContentLoaded', function(event) {
+   
+let dropdown = document.getElementById('dropdown');
+dropdown.addEventListener( "click" , function(event){
+	// alert ('клик работает')
+	dropdown.classList.remove('hide');
+	dropdown.classList.add('show')
+});
+
+let hide = document.getElementByClassName('hide');
+let body = document.getElementsByTagName('body')[0];
+body.addEventListener( "click" , function(event){
+	alert ('клик 2 работает')
+})
+
 });
